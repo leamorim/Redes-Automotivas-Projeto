e2070b856dcec4a1fbb562a94bb56b689c555d1b
Douglast1997 <dtrps@cin.ufpe.br>
2019-04-13 12:59:13 -0300

    Update teste_interrupt_button.ino
    ISR: Hard Sync e Soft Sync

--- teste_interrupt_button/teste_interrupt_button.ino
+++ teste_interrupt_button/teste_interrupt_button.ino
@@ -1,20 +1,31 @@
+#define button1 2 //Porta digital 2 para o botão 1
+#define button2 3 //Porta digital 3 para o botão 2
+
+// variables will change:
+volatile int buttonStateHardSync = 0;         // variable for reading the pushbutton status
+volatile int buttonStateSoftSync = 0;         // variable for reading the pushbutton status
+
 void setup() {
+ 
   Serial.begin(115200);
-  // put your setup code here, to run once:
-  attachInterrupt(0,buttone,RISING);
-  attachInterrupt(1,butttwo,RISING);
+  pinMode(button1, INPUT);  // initialize the pushbutton 1 pin as an input:
+  pinMode(button2, INPUT);  // initialize the pushbutton 2 pin as an input:
+
+  // Attach an interrupt to the ISR vector
+  attachInterrupt(0, HS_ISR, RISING);
+  attachInterrupt(1, SS_ISR, RISING);
 }
 
-void buttone(){
-  Serial.println("Interrupt Button ONE");
-}
-
-void butttwo(){
-  Serial.println("Interrupt Button TWO");
-}
 void loop() {
-  while(true){
-      Serial.print(".");
-      delay(250);
-  }  
+  // Nothing here!
+}
+
+void HS_ISR() {
+  buttonStateHardSync = digitalRead(buttonPin);
+  Serial.println("HARD SYNC")
+}
+
+void SS_ISR() {
+  buttonStateSoftSync = digitalRead(buttonPin);
+   Serial.println("SOFT SYNC")
 }
