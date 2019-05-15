17b854ee9b6eece891ccc7bb77c1140648830ce2
Belarmino <bgl@cin.ufpe.br>
2019-05-15 00:29:30 -0300

    Encoder com Remote Builder
    Bug no buffer, frame nao esta sendo mostrado no monitor corretamente (Nem o Data nem o Remote), pode ser problema na montagem ou na passagem do frame para o serial monitor

--- Encoder/Encoder.ino
+++ Encoder/Encoder.ino
@@ -1,4 +1,5 @@
 #include <TimerOne.h>
+#include <stdlib.h>
 
 //STATES
 
@@ -21,9 +22,10 @@
 
 #define TQ 1000000
 #define DATA_FRAME 0
+#define REMOTE_FRAME 1
 #define FF 0 
 #define FT 1
-#define FRAME_TYPE DATA_FRAME
+#define FRAME_TYPE REMOTE_FRAME
 
 
 
@@ -31,14 +33,15 @@
 //FT = 0 -> Data Frame / FT = 1 -> Remote Frame / FT = 2 -> Error Frame / FT = 3 -> Overload Frame
 
 volatile byte STATE;
-int count;
+int count = 0;
 char ID[11] = {'7','1','1','1','7','0','1','1','1','1','7'};
-char dlc[4] = {'D','L','C','1'};
-char crc[15] = {'1','2','3','4','5','6','7','8','9','1','2','3','4','5','6'};
-char data[8] = {'1','2','3','4','5','6','7','8'};
+char dlc[4] = {'D','L','C','E'};
+char crc[15] = {'3','3','3','3','3','3','3','8','3','3','3','3','3','3','9'};
+char data[8] = {'9','1','1','1','1','1','1','9'};
 int DLC_L = 1;
 //char *data =  malloc(sizeof(char) * (DLC_L*8));
 char *Frame = malloc(sizeof(char) * 55);
+char *printvec;
 
 void setup() {
   Serial.begin(9600);
@@ -49,11 +52,11 @@ void setup() {
 }
 
 void Frame_Printer(char*v){
-  Serial.print("FRAME:\n");
+  Serial.println("FRAME:");
   for(int i = 0;i <55;i++){
     Serial.print(v[i]);
-    Serial.print("\n");
   }
+  Serial.println();
 }
 
 void Inc_Count(){
@@ -71,113 +74,206 @@ void Data_Builder(){
       }
        Frame[0] = '0';
     Serial.print("SOF\n");
-    Serial.print(count);
-    Serial.print("\n");
     case ID_A:
-      Frame[count+1] = ID[count];
       if(count == 11){
         STATE = RTR;
         count = 0; 
-      } // 1 2 3 4 5 6 7 8 9 1 2 
-      break;
+        break;
+      }
+      Frame[count+1] = ID[count];
     case RTR:
       Frame[12] = '0';
       if(count == 1){
         STATE = IDE;
         count = 0; 
+        break;
       }
-      break;
     case IDE:
       Frame[13] = '0';
       if(count == 1){
         STATE = R0;
-        count = 0; 
+        count = 0;
+        break; 
       }
-      break;
     case R0:
       Frame[14] = '0';
       if(count == 1){
         STATE = DLC;
         count = 0; 
+        break;
       }
-      break;
     case DLC:
-      Serial.print("TO AQUI");
-      Frame[count + 15] = dlc[count];
-      if(count == 3){
+      if(count == 4){
         STATE = DATA;
         count = 0; 
+        break;
       }
-      break;
+      Frame[count + 15] = dlc[count];
     case DATA:
-    Serial.print("DATA");
-      Frame[count +19] = data[count];
-      if(count == DLC_L*8){
+      if(count == (DLC_L*8)){
         STATE = CRC;
         count = 0; 
+        break;
       }
-      break;
+      Frame[count +19] = data[count];
     case CRC:
-      Frame[count + 20 + DLC_L*8] = crc[count];
-      if(count == 15){
+      if(count == 16){
         STATE = CRC_DELIMITER;
         count = 0; 
+        break;
       }
-      break;
+      Frame[count + 19 + (DLC_L*8)] = crc[count];
+      //break;
     case CRC_DELIMITER:
-      Frame[count + 35 + DLC_L*8] = '1';
       if(count == 1){
         STATE = ACK_SLOT;
         count = 0; 
+        break;
       }
-      break;
+      Frame[count + 35 + (DLC_L*8)] = '1';
     case ACK_SLOT:
-      Frame[count + 36 + DLC_L*8] = '1';  // First Encoder writes RECESSIVE bit
       if(count == 1){
         STATE = ACK_DELIMITER;
         count = 0; 
+        break;
       }
-      break;
+      Frame[count + 36 + (DLC_L*8)] = '1';  // First Encoder writes RECESSIVE bit
     case ACK_DELIMITER:
-      Frame[count + 37 + DLC_L*8] = '1';
       if(count == 1){
         STATE = EOF;
         count = 0; 
+        break;
       }
-      break;
+      Frame[count + 37 + (DLC_L*8)] = '1';
     case EOF:
-      Frame[count + 38 + DLC_L*8] = '1';
-      if(count == 6){
+      if(count == 7){
         STATE = INTERFRAME_SPACING;
-        count = 0; 
+        count = 0;
+        break; 
       }
+      Frame[count + 44 + (DLC_L*8)] = '1';
     case INTERFRAME_SPACING:
-      Frame[count + 45 + (DLC_L*8)] = '1';
-      if(count == 3){
+      if(count == 4){
         count = 0; 
         STATE = SOF;
+        break;
       }
-      break;
+      Frame[count + 44 + (DLC_L*8)] = '1';
     case WAIT:
       break;     
       } 
   }
-void Remote_Builder();
+
+void Remote_Builder(){
+  Serial.print("Remote Builder\n");
+  switch(STATE){
+    case SOF:
+      if(count == 1){
+        STATE = ID_A;
+        count = 0; 
+        break;
+      }
+       Frame[0] = '0';
+       Serial.flush();
+       Serial.print("SOF\n");
+    case ID_A:
+      if(count == 11){
+        STATE = RTR;
+        count = 0; 
+        break;
+      } // 0 - 1234567891011
+      Frame[count+1] = ID[count];
+    case RTR:
+      Frame[12] = '0';
+      if(count == 1){
+        STATE = IDE;
+        count = 0; 
+        break;
+      }
+    case IDE:
+      Frame[13] = '0';
+      if(count == 1){
+        STATE = R0;
+        count = 0;
+        break; 
+      }
+    case R0:
+      Frame[14] = '0';
+      if(count == 1){
+        STATE = DLC;
+        count = 0; 
+        break;
+      }
+    case DLC:
+      if(count == 4){
+        STATE = DATA;
+        count = 0; 
+        break;
+      }
+      Frame[count + 15] = dlc[count];
+    case CRC:
+      if(count == 16){
+        STATE = CRC_DELIMITER;
+        count = 0; 
+        break;
+      }
+      Frame[count +19] = crc[count];
+      //break;
+    case CRC_DELIMITER:
+      if(count == 1){
+        STATE = ACK_SLOT;
+        count = 0; 
+        break;
+      }
+      Frame[count + 35] = '1';
+    case ACK_SLOT:
+      if(count == 1){
+        STATE = ACK_DELIMITER;
+        count = 0; 
+        break;
+      }
+      Frame[count + 36] = '1';  // First Encoder writes RECESSIVE bit
+    case ACK_DELIMITER:
+      if(count == 1){
+        STATE = EOF;
+        count = 0; 
+        break;
+      }
+      Frame[count + 37] = '1';
+    case EOF:
+      if(count == 7){
+        STATE = INTERFRAME_SPACING;
+        count = 0;
+        break; 
+      }
+      Frame[count + 44 + DLC_L*8] = '1';
+    case INTERFRAME_SPACING:
+      if(count == 4){
+        count = 0; 
+        STATE = SOF;
+        break;
+      }
+      Frame[count + 44 + (DLC_L*8)] = '1';
+    case WAIT:
+      break;     
+      } 
+  }
 void Error_Builder();
 void Overload_Builder();
 
 void Frame_Builder(){
+  // Base Frame Builders
   if(FF == 0){
     switch(FRAME_TYPE){
       case DATA_FRAME:
         Data_Builder();
         break;
 
-      /*case REMOTE_FRAME:
-        Remote_Builder()
+      case REMOTE_FRAME:
+        Remote_Builder();
         break;
 
-      case ERROR_FRAME:
+      /*case ERROR_FRAME:
         Error_Builder()
         break;
 
@@ -187,6 +283,7 @@ void Frame_Builder(){
         */
     }
   }
+  // Extended Frame Builders
   else{
     switch(FT){
       case DATA_FRAME:
@@ -210,9 +307,12 @@ void Frame_Builder(){
 }
 
 void loop() {
+  Serial.flush();
   Frame_Builder();
   count++;
+  Serial.flush();
   Frame_Printer(Frame);
-  delay(500);
+  Serial.flush();
+  //delay(500);
 }
   // put your main code here, to run repeatedly:
